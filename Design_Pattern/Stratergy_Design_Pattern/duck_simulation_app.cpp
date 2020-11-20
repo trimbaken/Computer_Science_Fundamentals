59a2b713299dbc3ba04d80e2aca5811db6cb4029
Nilesh Trimbake <trimbaken@gmail.com>
2020-11-20 15:06:05 +0530

    [DP][Stratergy] Implement Duck Simulation App - version 3.0
    This patch implememt Duck Simulation App,
    using stratergy design pattern.

--- Design_Pattern/Stratergy_Design_Pattern/duck_simulation_app.cpp
+++ Design_Pattern/Stratergy_Design_Pattern/duck_simulation_app.cpp
@@ -21,48 +21,131 @@ class Flyable {
 //		virtual void flyForShotDuration() =0;
 };
 
+class DontFly : public Flyable {
+	public:
+		void fly() {
+			cout<<"Don't Fly"<<endl;
+		}
+};
+
+class ShortFly : public Flyable {
+	public:
+		void fly() {
+			cout<<"Fly for short duration"<<endl;
+		}
+};
+
+class LongFly : public Flyable {
+	public:
+		void fly() {
+			cout<<"Fly for long duration"<<endl;
+		}
+};
+
+
 class Quackable {
 	public:
 		virtual void quack() =0;
 };
 
-class MallardDuck : public Duck, Flyable, Quackable {
+class Quack : public Quackable {
+	public:
+		void quack()
+		{
+			cout<<"Duck Quack "<<endl;
+		}
+};
+
+class DontQuck : public Quackable {
+	public:
+		void quack()
+		{
+			cout<<"Duck Don't Quack "<<endl;
+		}
+};
+
+
+
+class MallardDuck : public Duck {
+	private:
+		Flyable *flyable;
+		Quackable *quackable;
 	public:
 		void name()
 		{
 			cout<<"Duck Name : Mallard Duck"<<endl;
 		}
+
+		void set_flying_behaviour(Flyable *flyable_behaviour)
+		{
+			this->flyable = flyable_behaviour;
+		}
+
 		void fly()
 		{
-			cout<<"Mallard Duck Fly in sky"<<endl;
+			flyable->fly();
+		}
+		void set_quackabke_behaviour(Quackable *quackable_behaviour)
+		{
+			this->quackable = quackable_behaviour;
 		}
 		void quack()
 		{
-			cout<<"Mallard Duck quack"<<endl;
+			quackable->quack();
 		}
 };
 
 class RedheaddDuck : public Duck, Flyable, Quackable {
+	private:
+		Flyable *flyable;
+		Quackable *quackable;
 	public:
 		void name()
 		{
 			cout<<"Duck Name : Redhead Duck"<<endl;
 		}
+		void set_flying_behaviour(Flyable *flyable_behaviour)
+		{
+			this->flyable = flyable_behaviour;
+		}
 		void fly()
 		{
-			cout<<"Redhead Duck Fly in sky"<<endl;
+			flyable->fly();
+		}
+		void set_quackabke_behaviour(Quackable *quackable_behaviour)
+		{
+			this->quackable = quackable_behaviour;
 		}
 		void quack()
 		{
-			cout<<"Redhead Duck quack"<<endl;
+			quackable->quack();
 		}
 };
 
 class RabbardDuck : public Duck {
+	private:
+		Flyable *flyable;
+		Quackable *quackable;
 	public:
 		void name()
 		{
 			cout<<"Duck Name : Rabbar Duck"<<endl;
+		}
+		void set_flying_behaviour(Flyable *flyable_behaviour)
+		{
+			this->flyable = flyable_behaviour;
+		}
+		void fly()
+		{
+			flyable->fly();
+		}
+		void set_quackabke_behaviour(Quackable *quackable_behaviour)
+		{
+			this->quackable = quackable_behaviour;
+		}
+		void quack()
+		{
+			quackable->quack();
 		}
 };
 
@@ -74,6 +157,10 @@ int main(void)
 	cout<<endl;
 
 	MallardDuck mallardDuck;
+	ShortFly shortFly;
+	mallardDuck.set_flying_behaviour(&shortFly);
+	Quack quack;
+	mallardDuck.set_quackabke_behaviour(&quack);
 	mallardDuck.name();
 	mallardDuck.display();
 	mallardDuck.swim();
@@ -82,6 +169,9 @@ int main(void)
 	cout<<endl;
 
 	RedheaddDuck redheaddDuck;
+	LongFly longFLy;
+	redheaddDuck.set_flying_behaviour(&longFLy);
+	redheaddDuck.set_quackabke_behaviour(&quack);
 	redheaddDuck.name();
 	redheaddDuck.display();
 	redheaddDuck.swim();
@@ -90,9 +180,15 @@ int main(void)
 	cout<<endl;
 
 	RabbardDuck rabbardDuck;
+	DontFly dontFly;
+	rabbardDuck.set_flying_behaviour(&dontFly);
+	DontQuck dont_quack;
+	rabbardDuck.set_quackabke_behaviour(&dont_quack);
 	rabbardDuck.name();
 	rabbardDuck.display();
 	rabbardDuck.swim();
+	rabbardDuck.fly();
+	rabbardDuck.quack();
 	cout<<endl;
 
 	cout<<" Program to implement Duck Simulation App"<<endl;
