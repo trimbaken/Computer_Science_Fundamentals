d9e24e017563095e832763ade143abc49b647f61
Nilesh Trimbake <trimbaken@gmail.com>
2020-11-22 08:53:43 +0530

    [DP][Decorator] Implement Starbuzz Coffee Shop - version 2.0
    This patch implement starbuzz coffee shop logic using inheritance.
    
    Issue : any update (add\delete\modify) in ingredience cost, need to update base class.

--- Design_Pattern/Decorator_Design_Pattern/starbuzz_coffee_shop.cpp
+++ Design_Pattern/Decorator_Design_Pattern/starbuzz_coffee_shop.cpp
@@ -7,6 +7,10 @@ using namespace std;
 class Beverage {
 	private:
 		char description[100];
+		int milk;
+		int soy;
+		int mocha;
+		int whip;
 	public:
 		void set_description(char* new_description)
 		{
@@ -21,16 +25,60 @@ class Beverage {
 		}
 		void get_description()
 		{
-			cout<<"Beverage Description "<<description<<endl;
+			cout<<endl<<"Beverage Description: "<<endl<<description<<endl;
 		}
-		virtual void cost() = 0;
+
+		int has_milk()
+		{
+			if(milk == 1)
+				return 1;
+			else
+				return 0;
+		}
+		void set_milk()
+		{
+			milk =1;
+		}
+		int has_soy()
+		{
+			if(soy ==1)
+				return 1;
+			else
+				return 0;
+		}
+		void set_soy()
+		{
+			soy =1;
+		}
+		int has_mocha()
+		{
+			if(mocha ==1)
+				return 1;
+			else
+				return 0;
+		}
+		void set_mocha()
+		{
+			mocha =1;
+		}
+		int cost(){
+			int cost = 0;
+			if(has_milk())
+				cost = cost + 10;
+			if(has_soy())
+				cost = cost + 5;
+			if(has_mocha())
+				cost = cost + 15;
+			return cost;
+		}
 };
 
 class HouseBlend : public Beverage {
 	public:
 		void cost()
 		{
-			cout<<"Cost of HouseBlend Coffee is 10$ "<<endl;
+			int cost = Beverage::cost() + 10;
+			cout<<"Cost of HouseBlend Coffee is "<<cost<<"$"<<endl;
 		}
 };
 
@@ -38,7 +86,8 @@ class DarkRoast : public Beverage {
 	public:
 		void cost()
 		{
-			cout<<"Cost of DarkRoast Coffee is 15$ "<<endl;
+			int cost = Beverage::cost() + 15;
+			cout<<"Cost of DarkRoast Coffee is "<<cost<<"$"<<endl;
 		}
 };
 
@@ -46,7 +95,8 @@ class Expresso : public Beverage {
 	public:
 		void cost()
 		{
-			cout<<"Cost of Expresso Coffee is 20$ "<<endl;
+			int cost = Beverage::cost() + 20;
+			cout<<"Cost of Expresso Coffee is "<<cost<<"$"<<endl;
 		}
 };
 
@@ -57,18 +107,24 @@ int main(void)
 	HouseBlend house_blend;
 	char description[100] = "House Blend Coffee contain coffe + milk";
 	house_blend.set_description(description);
+	house_blend.set_milk();
+	house_blend.set_soy();
+	house_blend.set_mocha();
 	house_blend.get_description();
 	house_blend.cost();
 
 	DarkRoast dark_roast;
 	char dark_roast_description[100] = "Dark Roast Coffee contain coffe + milk + Chocolate";
 	dark_roast.set_description(dark_roast_description);
+	dark_roast.set_milk();
+	dark_roast.set_soy();
 	dark_roast.get_description();
 	dark_roast.cost();
 
 	Expresso expresso;
 	char expresso_description[100] = "Expresso Coffee contain coffe + milk + Sugar";
 	expresso.set_description(expresso_description);
+	expresso.set_mocha();
 	expresso.get_description();
 	expresso.cost();
 
