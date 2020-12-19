22201e2057ab2fa0a358eb713863634f61098b27
Nilesh Trimbake <trimbaken@gmail.com>
2020-12-19 21:21:20 +0530

    [DS][Graph] Graph Operations - Version 3.0
    This patch implement following operations:
    1. Print All Nodes
    2. Create Sample Graph
    3. Graph Traversal - DFS

--- Datastructures/Graph/Graph_Operations/graph.c
+++ Datastructures/Graph/Graph_Operations/graph.c
@@ -18,11 +18,20 @@ typedef struct graph_node_s
         struct adjacency_list_node_s* adjacency_list;
 }graph_node_t;
 
+typedef struct stack_s
+{
+	int stack_node_value;
+}stack_t;
+
+
 graph_node_t graph_node[MAX_NODES];
 adjacency_list_node_t adjacency_list_node[MAX_EDGES];
+stack_t stack_dfs[MAX_EDGES];
 
-int graph_node_count = -1;
+int graph_node_count = 0;
 int adjacency_list_node_count = -1;
+int stack_dfs_top =0;
+
 
 adjacency_list_node_t* create_adjacency_list_node(int adjacency_node_value, int weight)
 {
@@ -36,30 +45,20 @@ adjacency_list_node_t* create_adjacency_list_node(int adjacency_node_value, int 
 	return &adjacency_list_node[adjacency_list_node_count];
 }
 
-graph_node_t* create_graph_node(int graph_node_value)
-{
-	if(graph_node_count >= MAX_NODES)
-		return NULL;
-	graph_node_count++;
-	graph_node[graph_node_count].node_value = graph_node_value;
-	graph_node[graph_node_count].adjacency_list = NULL;
-
-	return &graph_node[graph_node_count];
-}
-
 int insert_edge(int start_node, int end_node, int weight)
 {
-//	printf("\n Insert Edge start_node : %d, end_node : %d, weight : %d \n", start_node, end_node, weight);
+	printf("\n Insert Edge start_node : %d, end_node : %d, weight : %d \n", start_node, end_node, weight);
 
 	if(graph_node[start_node].node_value == -1 || graph_node[start_node].adjacency_list == NULL)
 	{
 		graph_node[start_node].node_value = start_node;
 		graph_node[start_node].adjacency_list = create_adjacency_list_node(end_node, weight);
+		graph_node_count++;
 	}
 	else
 	{
 		adjacency_list_node_t *list_node = graph_node[start_node].adjacency_list;
-		while(list_node->next != NULL || list_node->adjacency_node_value != end_node)
+		while(list_node->next != NULL)
 		{
 			if(list_node->adjacency_node_value == end_node)
 			{
@@ -99,6 +98,7 @@ int delete_node(int node_index)
 	{
 		graph_node[node_index].node_value = -1;
 		graph_node[node_index].adjacency_list = NULL;
+		graph_node_count--;
 	}
 	return 1;
 }
@@ -174,6 +174,137 @@ int modify_edge(int start_node, int end_node, int weight, int new_weight)
 	return 0;
 }
 
+void print_graph_nodes()
+{
+	int number_of_nodes = 0;
+	int i =0;
+	printf("\nAll graph nodes :\n");
+	for(i =0; (i<MAX_NODES) && (number_of_nodes < graph_node_count); i++)
+	{
+		if(graph_node[i].node_value != -1)
+		{
+			number_of_nodes++;
+			printf(" Node : %d \n", graph_node[i].node_value);
+		}
+	}
+}
+
+void insert_bi_directional_edge(int start_node, int end_node, int weight)
+{
+	if(!insert_edge(start_node, end_node, weight))
+	{
+		printf("\nFail to insert edge\n");
+	}
+	if(!insert_edge(end_node, start_node, weight))
+	{
+		printf("\nFail to insert edge\n");
+	}
+}
+
+void create_graph()
+{
+	insert_bi_directional_edge(0, 1, 4);
+	insert_bi_directional_edge(0, 7, 8);
+	insert_bi_directional_edge(1, 2, 8);
+	insert_bi_directional_edge(1, 7, 11);
+	insert_bi_directional_edge(2, 3, 7);
+	insert_bi_directional_edge(2, 8, 2);
+	insert_bi_directional_edge(2, 5, 4);
+	insert_bi_directional_edge(3, 4, 9);
+	insert_bi_directional_edge(3, 5, 14);
+	insert_bi_directional_edge(5, 6, 2);
+	insert_bi_directional_edge(6, 8, 6);
+	insert_bi_directional_edge(6, 7, 1);
+	insert_bi_directional_edge(7, 8, 7);
+}
+
+int is_stack_dfs_empty()
+{
+	if(stack_dfs_top == 0)
+		return 1;
+	return 0;
+}
+
+int stack_dfs_push(int graph_node)
+{
+	printf("\n DFS Traverser Node [%d]\n", graph_node);
+	stack_dfs[stack_dfs_top++].stack_node_value = graph_node;
+	return 1;
+}
+
+int stack_dfs_pop()
+{
+	if(is_stack_dfs_empty())
+	{
+		printf("\nDFS Stack is Empty \n");
+		return -1;
+	}
+	return (stack_dfs[stack_dfs_top--].stack_node_value);
+}
+
+int get_stack_dfs_top_node()
+{
+	if(is_stack_dfs_empty())
+	{
+		printf("\nDFS Stack is Empty \n");
+		return -1;
+	}
+	return (stack_dfs[stack_dfs_top].stack_node_value);
+
+}
+
+int dfs(int start_node)
+{
+	int visited[MAX_NODES];
+	int i =0;
+	int pop_current_node =0;
+	int current_node =0;
+	adjacency_list_node_t* list_node = NULL;
+	if(!serach_node(start_node))
+	{
+		printf("\n Node not present  \n");
+		return 0;
+	}
+	for(i =0; i<MAX_NODES; i++)
+	{
+		visited[i] = 0;
+	}
+	if(stack_dfs_push(start_node) == -1)
+	{
+		printf("\n Fail to push into stack\n");
+		return 0;
+	}
+	visited[start_node] =1;
+	while(!is_stack_dfs_empty())
+	{
+		current_node = get_stack_dfs_top_node();
+		pop_current_node = 1;
+		list_node = graph_node[current_node].adjacency_list;
+		while(list_node != NULL)
+		{
+			if(visited[list_node->adjacency_node_value] == 0)
+			{
+				if(stack_dfs_push(list_node->adjacency_node_value) == -1)
+				{
+					printf("\nFail to push into stack\n");
+					return 0;
+				}
+				pop_current_node = 0;
+				visited[list_node->adjacency_node_value] =1;
+			}
+			list_node = list_node->next;
+		}
+		if(pop_current_node)
+		{
+			if(stack_dfs_pop() == -1)
+			{
+				printf("\nFail to pop from stack\n");
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
 
 void init()
 {
@@ -202,12 +333,14 @@ void init()
 		printf("4 Search Edge: \n");
 		printf("5 Delete Edge: \n");
 		printf("6 Modify Edge: \n");
-		printf("7 Print All Element: \n");
+		printf("7 Print All Nodes: \n");
+		printf("8 Create Graph: \n");
+		printf("9 Graph Traversal - DFS: \n");
 		printf("\n0 Exit: \n");
 
 		printf("\nEnter Operation Number:\n");
 		scanf("%d", &operation);
-		if(operation < 0 || operation > 7)
+		if(operation < 0 || operation > 9)
 		{
 			system("clear");
 			printf("\n Invalid operation selected  \n");
@@ -314,6 +447,24 @@ void init()
 			case 7:
 				{
 					printf("\n Print All Graph Nodes  \n");
+					print_graph_nodes();
+					break;
+				}
+			case 8:
+				{
+					printf("\n Create Graph  \n");
+					create_graph();
+					break;
+				}
+			case 9:
+				{
+					printf("\n Graph Traversal - DFS  \n");
+					printf("Enter start node :\n");
+					scanf("%d", &start_node);
+					if( !dfs(start_node))
+					{
+						printf("\nGraph traversal using DFS failed\n");
+					}
 					break;
 				}
 			case 0:
