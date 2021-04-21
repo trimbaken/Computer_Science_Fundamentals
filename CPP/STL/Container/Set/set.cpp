38fd8938392882275e37d591c5b03a34634edbd5
Nilesh Trimbake <trimbaken@gmail.com>
2021-04-21 08:27:17 +0530

    [CPP][STL] Set - version 1.0
    This patch implement functionalities of STL Set.

--- CPP/STL/Container/Set/set.cpp
+++ CPP/STL/Container/Set/set.cpp
@@ -12,6 +12,16 @@ void show_set(set<int>int_set)
 	return;
 }
 
+typedef struct person_s
+{
+	int id;
+	string name;
+}person_t;
+
+auto compare_person = [](person_t p1, person_t p2){
+	return p1.id<p2.id;
+};
+
 int main(void)
 {
 	set<int>int_set;
@@ -33,6 +43,27 @@ int main(void)
 	cout<<"\n find "<<*int_set.find(30);
 	cout<<"\n lower bound "<<*int_set.lower_bound(7);
 	cout<<"\n upper bound "<<*int_set.upper_bound(7);
+
+	set<person_t, decltype(compare_person)>person_set(compare_person);
+
+	person_t p1;
+	p1.id = 10;
+	p1.name = "abc";
+	person_t p2;
+	p2.id = 5;
+	p2.name = "abcd";
+	person_t p3;
+	p3.id = 20;
+	p3.name = "abcde";
+
+	person_set.insert(p1);
+	person_set.insert(p2);
+	person_set.insert(p3);
+
+	for(auto p : person_set)
+	{
+		cout<<"\n id "<<p.id <<" name "<<p.name;
+	}
 	cout<<"\n Program to implement functionalities of STL Set\n";
 	return 0;
 }
