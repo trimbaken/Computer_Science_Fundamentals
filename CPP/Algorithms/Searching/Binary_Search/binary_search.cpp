f0c31a2a9ae53eaa8147f9fecfa7284209571e8e
Nilesh Trimbake <trimbaken@gmail.com>
2021-04-24 14:53:45 +0530

    [CPP][Algorithms] Binary Search This program implement functionalities of BInary Searc

---
+++ CPP/Algorithms/Searching/Binary_Search/binary_search.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void show_a(int a[], int a_size)
+{
+	cout<<"\n Contents of a :\n";
+	for(int i =0; i<a_size; i++)
+	{
+		cout<<" "<<a[i];
+	}
+	return;
+}
+
+int main(void)
+{
+	int a[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+	int a_size = sizeof(a)/sizeof(a[0]);
+	show_a(a, a_size);
+	sort(a, a+a_size);
+	show_a(a, a_size);
+
+	if(binary_search(a, a+a_size, 10))
+		cout<<"\n element is present";
+	else
+		cout<<"\n element is not present";
+	cout<<"\n Program to implement binary search\n";
+	return 0;
+}
