e61796df2fc705450ecece11977bc4110cdb25f1
Nilesh Trimbake <trimbaken@gmail.com>
2021-04-24 20:31:30 +0530

    [CPP][Datastructure] String
    This patch implements different functions of string.

---
+++ CPP/Datastructures/String/string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string str = "ProgramOfTheDay";
+	string str1;
+	str1 = str.substr(2,3);
+	cout<<str.substr(2, 3);
+	cout<<"\n"<<str1;
+	cout<<"\n Program to implement functions of string \n";
+	return 0;
+}
