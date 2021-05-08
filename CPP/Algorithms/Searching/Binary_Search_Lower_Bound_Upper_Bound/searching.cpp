40335c3d0c8c80b61e9bef1d977253eb24daf608
Nilesh Trimbake <trimbaken@gmail.com>
2021-05-08 21:41:04 +0530

    [CPP][Algorithms] Searching version 2.
    This patch implement binary search using following algorithms with custome compare function
    1. binary_search
    2. lower_bound
    3. upper_bound

--- CPP/Algorithms/Searching/Binary_Search_Lower_Bound_Upper_Bound/searching.cpp
+++ CPP/Algorithms/Searching/Binary_Search_Lower_Bound_Upper_Bound/searching.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+bool compare(int a, int b)
+{
+	return a<b;
+}
 int main(void)
 {
 	// initializing vector of integers
@@ -19,13 +23,13 @@ int main(void)
 	vector<int> arr3 = {10, 15, 25, 30, 35};
 
 	/* binary_search */
-	if(binary_search(arr1.begin(), arr1.end(), 20))
+	if(binary_search(arr1.begin(), arr1.end(), 20, compare))
 		cout<<"\n 20 is present in arr1";
 	else
 		cout<<"\n 20 is not present in arr1";
 
 	/* lower_bound - given index of first element >= search element */
-	int lb = lower_bound(arr2.begin(), arr2.end(), 20)- arr2.begin();
+	int lb = lower_bound(arr2.begin(), arr2.end(), 20, compare)- arr2.begin();
 	if(arr2[lb] == 20)
 	{
 		cout<<"\n 20 is present at "<<lb;
@@ -39,7 +43,7 @@ int main(void)
 		cout<<"\n element not present in its lower bound is also not present";
 	}
 	/* upper_bound  - gives index of first element > search element*/
-	int ub = upper_bound(arr2.begin(), arr2.end(), 20)- arr2.begin();
+	int ub = upper_bound(arr2.begin(), arr2.end(), 20, compare)- arr2.begin();
 	cout<<"\n upper_bound index "<<ub;
 	if(ub>0 && arr2[ub-1] == 20)
 	{
