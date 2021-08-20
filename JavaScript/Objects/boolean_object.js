7f79161a46595182f30dc7d677efc463223fd63b
Nilesh Trimbake <trimbaken@gmail.com>
2021-08-20 08:13:24 +0530

    [JS][Objects] Boolean Object
    This program implement Boolean object, its properties
    and methods.

---
+++ JavaScript/Objects/boolean_object.js
@@ -0,0 +1,11 @@
+// The Boolean object represents two values, either "true" or "false"
+
+var true_boolean = Boolean(true);
+var false_boolean = Boolean(false);
+
+
+console.log("true_boolean :", true_boolean);
+console.log("false_boolean :", false_boolean);
+
+console.log("valueOf : ", true_boolean.valueOf());
+console.log("toString :", false_boolean.toString());
